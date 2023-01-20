15e5308839f48e1d08630c822411232609daf6f1
dlaperishvili <davitlaperishvili@gmail.com>
2023-01-21 01:20:08 +0400

    routing and link active class is fixed

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,24 +2,39 @@ import React, { useState } from "react";
 import "./menu.scss";
 import { NavLink } from "react-router-dom";
 export default function Menu() {
-    return (
-        <div className="left_section_navigation">
-            <nav className="navigation">
-                <ul>
-                    <li>
-                        <NavLink to="/portfolio-react">Web Projects</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/portfolio-react/experience">Experience</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/portfolio-react/other-information">Other Info</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/portfolio-react/contacts">Contacts</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+  const checkActive = (match, location) => {
+    //some additional logic to verify you are in the home URI
+    if (!location) return false;
+    const { pathname } = location;
+    console.log(pathname);
+    return pathname === "/";
+  };
+  console.log("Hello");
+  return (
+    <div className="left_section_navigation">
+      <nav className="navigation">
+        <ul>
+          <li>
+            <NavLink
+              to="/portfolio-react"
+              end
+            >
+              Web Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio-react/experience">Experience</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio-react/other-information">
+              Other Info
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio-react/contacts">Contacts</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -9,39 +9,61 @@ import WorkExp from "../WorkExp/WorkExp";
 import AdminAddExp from "../Admin/AdminAddExp";
 import AdminAddPortfolio from "../Admin/AdminAddPortfolio";
 export default function Menu(props) {
-    return (
-        <div className="main_content">
-            <section className="right_section">
-                <Routes>
-                    <Route exact path="/portfolio-react" element={<ProjectsList onProjectClick={props.onProjectClick} />}></Route>
-                    <Route path="/portfolio-react/experience" element={<WorkExp />}></Route>
-                    <Route path="/portfolio-react/other-information" element={<OtherInfo />}></Route>
-                    <Route path="/portfolio-react/contacts" element={<Contacts />}></Route>
+  return (
+    <div className="main_content">
+      <section className="right_section">
+        <Routes>
+          <Route
+            index
+            path="/portfolio-react"
+            element={<ProjectsList onProjectClick={props.onProjectClick} />}
+          ></Route>
+          <Route
+            path="/portfolio-react/experience"
+            element={<WorkExp />}
+          ></Route>
+          <Route
+            path="/portfolio-react/other-information"
+            element={<OtherInfo />}
+          ></Route>
+          <Route
+            path="/portfolio-react/contacts"
+            element={<Contacts />}
+          ></Route>
 
-                    {/* admin panel router */}
+          {/* admin panel router */}
 
-                    <Route path="admin" element={<Admin />}>
-                        <Route path="add-experience" element={<AdminAddExp />}></Route>
-                        <Route path="add-portfolio" element={<AdminAddPortfolio />}></Route>
-                        <Route
-                            path="*"
-                            element={
-                                <div>
-                                    <h3>No page found </h3>
-                                </div>
-                            }
-                        />
-                    </Route>
-                    <Route
-                        path="*"
-                        element={
-                            <div>
-                                <h3>No page found</h3>
-                            </div>
-                        }
-                    />
-                </Routes>
-            </section>
-        </div>
-    );
+          <Route
+            path="admin"
+            element={<Admin />}
+          >
+            <Route
+              path="add-experience"
+              element={<AdminAddExp />}
+            ></Route>
+            <Route
+              path="add-portfolio"
+              element={<AdminAddPortfolio />}
+            ></Route>
+            <Route
+              path="*"
+              element={
+                <div>
+                  <h3>No page found </h3>
+                </div>
+              }
+            />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <div>
+                <h3>No page found</h3>
+              </div>
+            }
+          />
+        </Routes>
+      </section>
+    </div>
+  );
 }
